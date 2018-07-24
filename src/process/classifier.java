2e0646a1608a09dfb1f13a9d3527306f78fc35d1
haoxingxing <mr.haoxx@gmail.com>
2018-07-24 14:52:42 +0800

    network update new process

--- src/process/classifier.java
+++ src/process/classifier.java
@@ -1,17 +1,37 @@
 package process;
 
+import network.protocol;
 public class classifier {
-    private String retrunbackstr;
+    private String returnbackstr;
 
-    public classifier(String command, String parameter) {
+    public classifier(protocol decoder) {
         int mode = 0;
+        if (decoder.getCommand().equals("ulog")) mode = 1;
+        if (decoder.getCommand().equals("ureg")) mode = 2;
         switch (mode) {
+            case 1:
+                if (decoder.PararmeterSize() == user.logpar) {
+                    if (user.login(decoder.getParameter()[0], decoder.getParameter()[1]))
+                        returnbackstr = "succ";
+                    else
+                        returnbackstr = "fall";
+                } else {
+                    returnbackstr = "erro";
+                }
+            case 2:
+                if (decoder.PararmeterSize() == user.regpar) {
+                    if (user.register(decoder.getParameter()[0], decoder.getParameter()[1]))
+                        returnbackstr = "succ";
+                    else
+                        returnbackstr = "fall";
+                } else {
+                    returnbackstr = "erro";
+                }
             default:
-                retrunbackstr = "cmnf";
+                returnbackstr = "E";
         }
     }
-
     public String returnback() {
-        return retrunbackstr;
+        return returnbackstr;
     }
 }
