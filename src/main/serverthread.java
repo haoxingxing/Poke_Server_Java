cfc80ef04b74495f1ca9589c21ba9374c1e94a4f
haoxingxing <mr.haoxx@gmail.com>
2018-08-01 08:07:11 +0800

    new md5 check

--- src/main/serverthread.java
+++ src/main/serverthread.java
@@ -1,6 +1,7 @@
 package main;
 
 import io.log;
+import io.md5;
 import network.protocol;
 import process.classifier;
 
@@ -8,6 +9,7 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Objects;
 
 class serverthread extends Thread {
     private Thread t;
@@ -29,29 +31,39 @@ class serverthread extends Thread {
             StringBuilder recvstr = new StringBuilder();
             try {
                 recvstr.append(recv.readLine());
-                while (!recvstr.toString().substring(recvstr.length() - 1).equals("\u0004")) {
+                while (!recvstr.toString().substring(recvstr.toString().length() - 1).equals("\u0005")) {
                     recvstr.append(recv.readLine());
                 }
-                try {
-                    decoder = new protocol(recvstr.toString());
-                } catch (IllegalArgumentException e) {
-                    log.printf("Error protocol");
-                    decoder = new protocol("ZXJyZg==");
+                recvstr.delete(recvstr.length() - 1, recvstr.length());
+                if (!recvstr.toString().split("\u0004")[1].equals(Objects.requireNonNull(md5.md5_encode(recvstr.toString().split("\u0004")[0])).toLowerCase()))
                     error = true;
-                }
+                String r = recvstr.toString().split("\u0004")[0];
                 if (!error) {
-                    if (!decoder.getCommand().equals("exit")) {
-                        log.printf("[" + sockettoclient.getRemoteSocketAddress().toString() + "]" + recvstr + " [" + decoder.getCommand() + "](" + decoder.getAllParameter() + ")");
-                        classifier process = new classifier(decoder);
-                        send.write(Base64.getEncoder().encodeToString(process.returnback().getBytes(StandardCharsets.UTF_8)));
-                        log.printf("To[" + sockettoclient.getRemoteSocketAddress().toString() + "]" + " [" + process.returnback() + "]");
+                    try {
+                        decoder = new protocol(r);
+                    } catch (IllegalArgumentException e) {
+                        log.printf("Error protocol");
+                        decoder = new protocol("ZXJyZg==");
+                        error = true;
+                    }
+
+                    if (!error) {
+                        if (!decoder.getCommand().equals("exit")) {
+                            log.printf("[" + sockettoclient.getRemoteSocketAddress().toString() + "]" + recvstr + " [" + decoder.getCommand() + "](" + decoder.getAllParameter() + ")");
+                            classifier process = new classifier(decoder);
+                            send.write(Base64.getEncoder().encodeToString(process.returnback().getBytes(StandardCharsets.UTF_8)));
+                            log.printf("To[" + sockettoclient.getRemoteSocketAddress().toString() + "]" + " [" + process.returnback() + "]");
+                            send.write("\u0004");
+                            send.flush();
+                        } else {
+                            sockettoclient.close();
+                        }
+                    } else {
                         send.write("\u0004");
                         send.flush();
-                    } else {
-                        sockettoclient.close();
                     }
                 } else {
-                    send.write("\u0004");
+                    send.write("\u0006\u0004");
                     send.flush();
                 }
             } catch (StringIndexOutOfBoundsException e) {
